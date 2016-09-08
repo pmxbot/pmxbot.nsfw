80f6875ef08cb3042259c703290abfe176d5cd5f
Jason R. Coombs <jaraco@jaraco.com>
2016-09-08 18:13:01 -0400

    Add tests

--- pmxbot/nsfw.py
+++ pmxbot/nsfw.py
@@ -4,6 +4,13 @@ import pmxbot
 
 
 def is_safe(channel, message):
+	"""
+	>>> pmxbot.config = {'exclude patterns': ['offensive word']}
+	>>> is_safe('#inane', 'some phrase')
+	True
+	>>> is_safe('#inane', "don't any of you say an offensive word")
+	False
+	"""
 	return not any(
 		re.search(pattern, message)
 		for pattern in pmxbot.config.get('exclude patterns', [])
