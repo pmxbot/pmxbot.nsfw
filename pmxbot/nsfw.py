45fd5b3243865e5b03c9ac1b28a432801e8ded07
Jason R. Coombs <jaraco@jaraco.com>
2022-07-01 20:20:47 -0400

    ⚫ Fade to black.

--- pmxbot/nsfw.py
+++ pmxbot/nsfw.py
@@ -4,14 +4,14 @@ import pmxbot
 
 
 def is_safe(channel, message):
-	"""
-	>>> pmxbot.config = {'exclude patterns': ['offensive word']}
-	>>> is_safe('#inane', 'some phrase')
-	True
-	>>> is_safe('#inane', "don't any of you say an offensive word")
-	False
-	"""
-	return not any(
-		re.search(pattern, message)
-		for pattern in pmxbot.config.get('exclude patterns', [])
-	)
+    """
+    >>> pmxbot.config = {'exclude patterns': ['offensive word']}
+    >>> is_safe('#inane', 'some phrase')
+    True
+    >>> is_safe('#inane', "don't any of you say an offensive word")
+    False
+    """
+    return not any(
+        re.search(pattern, message)
+        for pattern in pmxbot.config.get('exclude patterns', [])
+    )
